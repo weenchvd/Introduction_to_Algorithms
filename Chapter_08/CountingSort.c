9aaaf9b3a99f7981e46659c8f9f4a15bb2212b6e
weenchvd <d88744@gmail.com>
2019-10-23 22:31:22 +0300

    Chapter 8.2 | Counting-Sort

---
+++ Chapter_08/CountingSort.c
@@ -0,0 +1,33 @@
+﻿#include "CountingSort.h"
+#include <stdlib.h>
+#include <stdio.h>  
+
+/* nondecreasing order: type = 1
+   nonincreasing order: type = 0 */
+void CountingSort(int* a, int* b, int size, int k)
+{
+	int* c;
+	int i, j;
+	if ((c = malloc(sizeof(int) * ((size_t)k + 1))) == NULL) {
+		printf("\n\n\t| ERROR | Memory allocator error. No memory allocated (CountingSort()) |\n");
+		return;
+	}
+	for (i = 0; i <= k; i++) {
+		c[i] = 0;
+	}
+	for (j = 0; j < size; j++) {
+		c[a[j]]++;
+	}
+	for (i = 1; i <= k; i++) {
+		c[i] = c[i] + c[i - 1];
+	}
+	for (i = 0; i <= k; i++) {
+		c[i]--;
+	}
+	for (j = size - 1; j >= 0; j--) {
+		b[c[a[j]]] = a[j];
+		c[a[j]]--;
+	}
+	free(c);
+	return;
+}
---
+++ Chapter_08/CountingSort.h
@@ -0,0 +1,8 @@
+﻿#ifndef _COUNTINGSORT_H
+#define _COUNTINGSORT_H
+
+#include "CountingSort_common.h"
+
+void CountingSort(int* a, int* b, int size, int k);
+
+#endif
